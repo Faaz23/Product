1877ee9802f2b7bf38e89f45aef1ee5c347fc682
Faaz23 <136075768+Faaz23@users.noreply.github.com>
2024-07-04 12:29:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 1300,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
